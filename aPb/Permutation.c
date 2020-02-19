3d97b8955d904d9c01650f03828a59445d4fe140
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2020-02-18 18:08:45 -0600

    funcion para permutar cadenas

--- aPb/Permutation.c
+++ aPb/Permutation.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+
+#include <stdlib.h>
 #include "PermutationLibrary.h"
 #include "Sort.h"
 
 int main(){
 
 	int prueba [5] = {4,3,1,5,2};
-	
-	inversePermutation(5, prueba);
+	int * x = malloc(sizeof(int) * 5);
+	char h [6] = "hola ";
+	int i;
+	//x = inversePermutation(5, prueba);
+	//printf("_____________ inversa guardada\n");
+	for(i = 0; i< 5; i++){
+		printf("%d, ", prueba[i]);
+	}
+
+	printf("\nmensaje con prueba a cifrar con no inversa: %s\n", h);
+
+	permuteString(h, 5, prueba);
+		printf("Permutada:%s|\n", h);
+
 	return 0;
 }
--- aPb/PermutationLibrary.c
+++ aPb/PermutationLibrary.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Sort.h"
 #include "PermutationLibrary.h"
-
 
 int * inversePermutation(int n, int  permutation[n]){
 	int * inverse = malloc(sizeof(int)*n);
@@ -23,8 +23,19 @@ int * inversePermutation(int n, int  permutation[n]){
 	printPermutation(n, inverseAux);
 	//ordenar valores
 	for(i = 0; i < n; i++)
-		inverse[i] = inverseAux[i][0]; 
+		inverse[i] = inverseAux[i][0] + 1; 
 	return inverse;
+}
+
+//string.length == n
+void permuteString(char * string, int n, int permutation[n]){
+	int i;
+	char * auxString = malloc(sizeof(char) * n);
+	memcpy(auxString + 0, string + 0, n);
+ 	char c;
+ 	for(i = 0; i < n; i++){
+		string[i] = auxString[permutation[i]-1];
+	}
 }
 
 void printPermutation(int n, int permutation[n][2]){
