11d9fdf1e365c225ec18e35ce607360a85600dd8
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2020-05-11 11:52:07 -0500

    implementaicon de logaritmo discreto con conlisiones

--- logarithmfactors/discrete.c
+++ logarithmfactors/discrete.c
@@ -18,18 +18,43 @@ long powmod(long x, long y, long p) {
     } 
     return res; 
 } 
+
+long discreteLogarithm(long a, long m, long res){
+    long potencia = 0;
+    long i, j;
+    long n = sqrt(m) + 1;
+    long arr [n];
+    for(i = n; i >= 1 ; i--){
+        arr[i-1] = powmod (a, i * n, m);
+    }
+
+    for (j = 0; j < n; j++) { 
+        // Calculate (a ^ j) * b and check 
+        // for collision 
+        int cur = (powmod (a, j, m) * res) % m; 
+  
+        // If collision occurs i.e., LHS = RHS 
+        for(i = 0; i < n; i++){
+           if(cur == arr[i]){
+                potencia = (i+1) * n - j;
+                // Check whether ans lies below m or not 
+                if (potencia < m) 
+                    return potencia; 
+           } 
+        }
+    } 
+    return 0;
+}
+
 int main(int argc, char const *argv[]){
     long i;
     long a, m, res;
     a = 3;
     m = 500000009;
     res = 406870124;
-    for(i = 0; i < m; i ++){
-        if(powmod(a, i, m) == res){
-            printf("i = %ld\n", i);
-            break;
-        }
-    }
+    
+    i = discreteLogarithm(a, m, res);
+
     printf("%ld^xmod%ld = %ld; x =%ld\n", a, m, res, i);
     return 0;
 }
