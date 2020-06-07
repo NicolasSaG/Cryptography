cb94fc16432033c61aa89b63f20facd58f52cc9c
Nicolas Sánchez <fsanchezg1403@alumno.ipn.mx>
2020-06-07 18:34:55 -0500

    sha256 primitiva

--- proyecto/sha256.c
+++ proyecto/sha256.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <openssl/sha.h>
 
 static const int K_READ_BUF_SIZE = { 1024 * 16 };
 
-int main(int argc, char const *argv[]){
-    unsigned char * shaDigest = calculateSHA1("a.txt"); 
+//gcc sha256.c -lssl -lcrypto -o sha256
+unsigned char * calculateSHA1(char * filename);
 
-    char *sha1hash = (char *)malloc(sizeof(char) * 41);
-    sha1hash[41] = '\0';
+int main(int argc, char *argv[]){
+    unsigned char * shaDigest = calculateSHA256(argv[1]); 
+
+    char *sha256hash = (char *)malloc(sizeof(char) * 65);
+    sha256hash[65] = '\0';
     int i;
-    for (i = 0; i < SHA_DIGEST_LENGTH; i++){
-        sprintf(&sha1hash[i*2], "%02x", shaDigest[i]);
+    for (i = 0; i < SHA256_DIGEST_LENGTH; i++){
+        sprintf(&sha256hash[i*2], "%02x", shaDigest[i]);
     }
-    printf("SHA1 HASH: %s\n", sha1hash);
+    printf("SHA256 hash: %s\n", sha256hash);
     
     return 0;
 }
     //sha1 40 char 459567d3bde4418b7fe302ff9809c4b0befaf7dd
     //sha256 64 char 0b894166d3336435c800bea36ff21b29eaa801a52f584c006c49289a0dcf6e2f
 
-unsigned char * calculateSHA1(char * filename){
+unsigned char * calculateSHA256(char * filename){
     if (!filename) {
         return NULL;
     }
@@ -29,23 +33,23 @@ unsigned char * calculateSHA1(char * filename){
         return NULL;
     }
 
-    unsigned char * sha1_digest = malloc(sizeof(char) * SHA_DIGEST_LENGTH);
-    SHA_CTX context;
+    unsigned char * sha256_digest = malloc(sizeof(char) * SHA256_DIGEST_LENGTH);
+    SHA256_CTX context;
 
-    if(!SHA1_Init(&context))
+    if(!SHA256_Init(&context))
         return NULL;
 
     unsigned char buf[K_READ_BUF_SIZE];
     while (!feof(fp)){
         size_t total_read = fread(buf, 1, sizeof(buf), fp);
-        if(!SHA1_Update(&context, buf, total_read)){
+        if(!SHA256_Update(&context, buf, total_read)){
             return NULL;
         }
     }
     fclose(fp);
 
-    if(!SHA1_Final(sha1_digest, &context))
+    if(!SHA256_Final(sha256_digest, &context))
         return NULL;
 
-    return sha1_digest;
+    return sha256_digest;
 }
